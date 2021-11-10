cb35e09782251d2f31531321a653ab248c6f51f7
develhopper <alireza.tjd77@gmail.com>
2021-11-10 20:46:26 +0330

    first commit

---
+++ app/src/main/java/ir/code4life/willy/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package ir.code4life.willy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.code4life.willy.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
